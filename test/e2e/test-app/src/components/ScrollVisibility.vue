5cd72ef99eefe9f1cc7adaa4a4c1a01e43067e95
Chris Hurlburt <churlburt132@g.rwu.edu>
2017-12-14 23:08:38 -0500

    v3.0.1: bugfixes, logo update

---
+++ test/e2e/test-app/src/components/ScrollVisibility.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="['scroll-visibility', $vnode.key, { visible }]">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollVisibility',
+  props: {
+    visible: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+}
+</script>
+
+<style scoped>
+.scroll-visibility {
+  height: 350px;
+  width: 350px;
+  background: red;
+  margin-bottom: 1000px;
+  transition: background .2s ease-in-out;
+}
+
+.scroll-visibility.visible {
+  background: blue;
+}
+</style>
